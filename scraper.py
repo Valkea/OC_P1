49081d821a48038efc4b12e3b391dff5221ddfcf
Valkea <github-sub@shedge.com>
2020-11-30 19:43:34 +0100

    feat(book): add scraping methods to the Book class for the requested attributes (product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url)

--- scraper.py
+++ scraper.py
@@ -1,34 +1,163 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-'''
-The purpose of this module is to scrape the content 
-of the http://books.toscrape.com/ website.
+''' The purpose of this module is to scrape the content of
+    the http://books.toscrape.com/ website.
 '''
 
+import re
+from urllib.request import urlopen
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+
+
 ##################################################
-### Book
+# Generic
 ##################################################
+
+
+def connect_with_bs4(url):
+    """ Connect to the given URL, collect the html data
+        and return a BeautifulSoup object to work with
+
+    Parameters
+    ----------
+    url : str
+        The internet address to use in order to collect the data
+
+    Returns
+    -------
+    BeautifulSoup
+        An object containing parsed html data
+    """
+
+    page = urlopen(url)
+    html = page.read().decode('utf8')
+    soup = BeautifulSoup(html, 'html.parser')
+
+    return soup
+
+##################################################
+# Book
+##################################################
+
 
 class Book():
+    """ The purpose of this class is to collect
+        and store the product information
+    """
 
     def __init__(self, url):
-        self.Iproduct_page_url = url
-        self.Iuniversal_product_code = None
-        self.Ititle = None
-        self.Iprice_including_tax = None
-        self.Iprice_excluding_tax = None
-        self.Inumber_available = None
-        self.Iproduct_description = None
-        self.Icategory = None
-        self.Ireview_rating = None
-        self.Iimage_url = None
+        """
+        Parameters
+        ----------
+        url : str
+            The internet address of the product page
+        """
+
+        self.product_page_url = url
+        self.universal_product_code = None
+        self.title = None
+        self.price_including_tax = None
+        self.price_excluding_tax = None
+        self.number_available = None
+        self.product_description = None
+        self.category = None
+        self.review_rating = None
+        self.image_url = None
+
+    def collect(self):
+        """ Connect to the product page and grab the information """
+
+        self.soup = connect_with_bs4(self.product_page_url)
+
+        self.universal_product_code = self.__scrap_upc()
+        self.title = self.__scrap_title()
+        self.price_including_tax = self.__scrap_price_inc_tax()
+        self.price_excluding_tax = self.__scrap_price_exc_tax()
+        self.number_available = self.__scrap_number_available()
+        self.product_description = self.__scrap_product_description()
+        self.category = self.__scrap_category()
+        self.review_rating = self.__scrap_review_rating()
+        self.image_url = self.__scrap_image_url()
+
+    def __scrap_upc(self):
+        try:
+            return self.soup.find('th', string='UPC').find_next('td').string
+        except Exception:
+            return ''
+
+    def __scrap_title(self):
+        try:
+            return self.soup.find('h1').string
+        except Exception:
+            return ''
+
+    def __scrap_price_inc_tax(self):
+        try:
+            return self.soup.find('th', string="Price (incl. tax)") \
+                            .find_next('td').string
+        except Exception:
+            return ''
+
+    def __scrap_price_exc_tax(self):
+        try:
+            return self.soup.find('th', string="Price (excl. tax)") \
+                            .find_next('td').string
+        except Exception:
+            return ''
+
+    def __scrap_number_available(self):
+        try:
+            number_available_txt = self.soup.find('th', string='Availability')\
+                                            .find_next('td').string
+
+            return int(re.search(r'[0-9]+', number_available_txt).group())
+        except Exception:
+            return ''
+
+    def __scrap_product_description(self):
+        try:
+            return self.soup.select("#product_description")[0]  \
+                            .find_next('p').string
+        except Exception:
+            return ''
+
+    def __scrap_category(self):
+        try:
+            return self.soup.find('ul', class_='breadcrumb')    \
+                            .findAll('a')[2].string
+        except Exception:
+            return ''
+
+    def __scrap_review_rating(self):
+        try:
+            trans = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5}
+            return trans[(
+                self.soup.find('p', class_='star-rating')
+                .attrs['class'][1])]
+        except Exception:
+            return ''
+
+    def __scrap_image_url(self):
+        try:
+            relative_url = self.soup.img.attrs['src']  # relative
+            base_url = urljoin(self.product_page_url, '.')
+            return urljoin(base_url, relative_url)  # absolute
+        except Exception as e:
+            return e
+
+
+prod_url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+book = Book(prod_url)
+book.collect()
 
 ##################################################
-### Category
+# Category
 ##################################################
 
 
 ##################################################
-### Site
+# Site
 ##################################################
--- test_scraper.py
+++ test_scraper.py
@@ -2,76 +2,131 @@
 # coding: utf-8
 
 '''
-The purpose of this module is to test the scrape.py scraipping module.
+The purpose of this module is to test the scrape.py scraping module
 '''
 
 import pytest
 
-from scraper import Book
+from bs4 import BeautifulSoup
+
+from scraper import Book, connect_with_bs4
 
 
 ##################################################
-### Generic
+# Generic
 ##################################################
+
+def test_connect_with_bs4_TYPE():
+    url = 'http://books.toscrape.com'
+    assert type(connect_with_bs4(url)) == BeautifulSoup
+
+
+def test_connect_with_bs4_ERROR():
+
+    with pytest.raises(Exception):
+        connect_with_bs4('http://www.xxxfakexxx.xxx')
 
 
 ##################################################
-### Book
+# Book
 ##################################################
 
 class TestBook():
 
     def setup_method(self):
         fake_url = 'http://www.fake.url'
-        self.book = Book(fake_url)
+        self.book1 = Book(fake_url)
 
-    def test_prodduct_page_url(self):
-        self.book.product_page_url = "fake-url"
-        assert self.book.product_page_url == "fake-url"
+        prod_url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+        self.book2 = Book(prod_url)
+        self.book2.collect()
+
+    def test_empty_init(self):
+        with pytest.raises(Exception):
+            Book()
+
+    # --- Getters & Setters ---
+
+    def test_product_page_url(self):
+        self.book1.product_page_url = "fake-url"
+        assert self.book1.product_page_url == "fake-url"
 
     def test_universal_product_code(self):
-        self.book.universal_product_code = "fake-upc"
-        assert self.book.universal_product_code == "fake-upc"
+        self.book1.universal_product_code = "fake-upc"
+        assert self.book1.universal_product_code == "fake-upc"
 
     def test_title(self):
-        self.book.title = "fake-title"
-        assert self.book.title == "fake-title"
+        self.book1.title = "fake-title"
+        assert self.book1.title == "fake-title"
 
     def test_price_including_tax(self):
-        self.book.price_including_tax = 42.42
-        assert self.book.price_including_tax == 42.42
+        self.book1.price_including_tax = 42.42
+        assert self.book1.price_including_tax == 42.42
 
     def test_price_excluding_tax(self):
-        self.book.price_excluding_tax = 41.41
-        assert self.book.price_excluding_tax == 41.41
+        self.book1.price_excluding_tax = 41.41
+        assert self.book1.price_excluding_tax == 41.41
 
     def test_number_available(self):
-        self.book.number_available = 40
-        assert self.book.number_available == 40
+        self.book1.number_available = 40
+        assert self.book1.number_available == 40
 
     def test_product_description(self):
-        self.book.product_description = "fake-desc"
-        assert self.book.product_description == "fake-desc"
+        self.book1.product_description = "fake-desc"
+        assert self.book1.product_description == "fake-desc"
 
     def test_category(self):
-        self.book.category = "fake-cat"
-        assert self.book.category == "fake-cat"
+        self.book1.category = "fake-cat"
+        assert self.book1.category == "fake-cat"
 
     def test_review_rating(self):
-        self.book.review_rating = 10
-        assert self.book.review_rating == 10
+        self.book1.review_rating = 10
+        assert self.book1.review_rating == 10
 
     def test_image_url(self):
-        self.book.image_url = "fake-img-url"
-        assert self.book.image_url == "fake-img-url"
+        self.book1.image_url = "fake-img-url"
+        assert self.book1.image_url == "fake-img-url"
 
-                
+    # --- Collect data ---
+
+    def test_parse_product_infos(self):
+        url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+        assert self.book2.product_page_url == url
+
+    def test_parse_universal_product_code(self):
+        assert self.book2.universal_product_code == "a897fe39b1053632"
+
+    def test_parse_title(self):
+        assert self.book2.title == "A Light in the Attic"
+
+    def test_parse_price_including_tax(self):
+        assert self.book2.price_including_tax == "£51.77"
+
+    def test_parse_price_excluding_tax(self):
+        assert self.book2.price_excluding_tax == "£51.77"
+
+    def test_parse_number_available(self):
+        assert self.book2.number_available == 22
+
+    def test_parse_product_description(self):
+        assert self.book2.product_description == '''It's hard to imagine a world without A Light in the Attic. This now-classic collection of poetry and drawings from Shel Silverstein celebrates its 20th anniversary with this special edition. Silverstein's humorous and creative verse can amuse the dowdiest of readers. Lemon-faced adults and fidgety kids sit still and read these rhythmic words and laugh and smile and love th It's hard to imagine a world without A Light in the Attic. This now-classic collection of poetry and drawings from Shel Silverstein celebrates its 20th anniversary with this special edition. Silverstein's humorous and creative verse can amuse the dowdiest of readers. Lemon-faced adults and fidgety kids sit still and read these rhythmic words and laugh and smile and love that Silverstein. Need proof of his genius? RockabyeRockabye baby, in the treetopDon't you know a treetopIs no safe place to rock?And who put you up there,And your cradle, too?Baby, I think someone down here'sGot it in for you. Shel, you never sounded so good. ...more'''
+
+    def test_parse_category(self):
+        assert self.book2.category == "Poetry"
+
+    def test_parse_review_rating(self):
+        assert self.book2.review_rating == 3
+
+    def test_parse_image_url(self):
+        url = "http://books.toscrape.com/media/cache/fe/72/fe72f0532301ec28892ae79a629a293c.jpg"
+        assert self.book2.image_url == url
+
 
 ##################################################
-### Category
+# Category
 ##################################################
 
 
 ##################################################
-### Site
+# Site
 ##################################################
