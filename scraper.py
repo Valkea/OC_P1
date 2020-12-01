8b5473052479abff5b815d1cb8d328c63a77b6d2
Valkea <github-sub@shedge.com>
2020-12-01 23:57:09 +0100

    refactor: create a main function in scraper.py in order to regroup the local tests and speedup the pytest processes

--- scraper.py
+++ scraper.py
@@ -231,13 +231,6 @@ class Book():
             return e
 
 
-prod_url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-book = Book(prod_url)
-book.to_csv('OneProductAppend')
-book.collect()
-book.to_csv('OneProductAlone')
-book.to_csv('OneProductAppend')
-
 ##################################################
 # Category
 ##################################################
@@ -359,12 +352,25 @@ class Category:
         return books
 
 
-cat_url = 'http://books.toscrape.com/catalogue/category/books/fiction_10/index.html'
-cat1 = Category(cat_url)
-cat1.to_csv("cat1")
-cat1.to_csv("cat1")
-
-
 ##################################################
 # Site
 ##################################################
+
+
+##################################################
+# Main
+##################################################
+
+if __name__ == '__main__':
+
+    prod_url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+    book = Book(prod_url)
+    book.to_csv('OneProductAppend')
+    book.collect()
+    book.to_csv('OneProductAlone')
+    book.to_csv('OneProductAppend')
+
+    cat_url = 'http://books.toscrape.com/catalogue/category/books/fiction_10/index.html'
+    cat1 = Category(cat_url)
+    cat1.to_csv("cat1")
+    cat1.to_csv("cat1")
